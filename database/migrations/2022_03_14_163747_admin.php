9597576af48ca0c6dac5f0aa02ed61819f15b73b
ahmed <ahmed@gmail.com>
2022-03-16 13:20:40 +0200

    modify in migrate file

--- database/migrations/2022_03_14_163747_admin.php
+++ database/migrations/2022_03_14_163747_admin.php
@@ -19,7 +19,7 @@ class Admin extends Migration
 			$table->string('name');
 			$table->string('email');
             $table->string('password');
-            $table->string('remember_token');
+            $table->string('remember_token')->default(null);
 			$table->timestamps(6);
 		});
     }
